053f74d60774ce1aaafe05e9179c927f7c1d6ca6
tds-andre <tds.andre@gmail.com>
2015-08-14 21:02:37 -0300

    zero

---
+++ syst/Fgmaiss/impl/wrk/Tds.Prjs.Fgmaiss.JoinSal.UI/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tds.Prjs.Fgmaiss.JoinSal;
+
+namespace Tds.Prjs.Fgmaiss.JoinSal.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = new Manager();
+            x.Execute("C:\\_\\Projetos\\3 Rios\\syst\\Fgmaiss\\srvc\\data\\Salarios dos Servidores.pdf");
+            
+        }
+
+    }
+}
