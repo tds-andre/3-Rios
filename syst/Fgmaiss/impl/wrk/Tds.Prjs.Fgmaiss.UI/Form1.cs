0fe44c6a56781ae3e2f31a061c3e4a08cf0669be
tds-andre <tds.andre@gmail.com>
2015-09-23 23:47:25 -0300

    \sdas

--- syst/Fgmaiss/impl/wrk/Tds.Prjs.Fgmaiss.UI/Form1.cs
+++ syst/Fgmaiss/impl/wrk/Tds.Prjs.Fgmaiss.UI/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tds.Prjs.Fgmaiss.ParMed;
+using Tds.Prjs.Fgmaiss.Trash;
 
 
 namespace Tds.Prjs.Fgmaiss.UI
@@ -21,8 +22,14 @@ namespace Tds.Prjs.Fgmaiss.UI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var x = new Manager();
+            var x = new ParMed.Manager();
             x.Execute("..\\..\\..\\..\\..\\repo\\Emissão de Medicamentos - Almoxarifado.pdf.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var x = new Trash.Manager();
+            x.Execute("..\\..\\..\\..\\..\\repo\\Rotas Lixo Domiciliar Três Rios.pdf");
         }
     }
 }
