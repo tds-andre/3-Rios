2302e70c255693412cb26c2c74d7fdcc34ba67f1
tds-andre <tds.andre@gmail.com>
2015-12-15 18:30:46 -0200

    parser de servidores demitidos

--- syst/Fgmaiss/impl/wrk/Tds.Prjs.Fgmaiss.UI/Form1.cs
+++ syst/Fgmaiss/impl/wrk/Tds.Prjs.Fgmaiss.UI/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tds.Prjs.Fgmaiss.ParMed;
 using Tds.Prjs.Fgmaiss.Trash;
+using Tds.Prjs.Fgmaiss.ParDemission;
 
 
 namespace Tds.Prjs.Fgmaiss.UI
@@ -30,6 +31,12 @@ namespace Tds.Prjs.Fgmaiss.UI
         {
             var x = new Trash.Manager();
             x.Execute("..\\..\\..\\..\\..\\repo\\Rotas -  Varrição Equipe.pdf.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var x = new ParDemission.Executor();
+            x.Execute("..\\..\\..\\..\\..\\repo\\FGMAISS - Relação de Servidores Demitidos - 01_10 a 15_12_2015.pdf");
         }
     }
 }
